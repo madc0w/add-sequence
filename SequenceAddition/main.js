b90988ce338422edc82f7f4c90d28b9572b01199
Marc A. Donis <mad7@runbox.com>
2020-02-02 23:42:53 +0100

    .

---
+++ SequenceAddition/main.js
@@ -0,0 +1,14 @@
+var seq = [[0, 1], [1]];
+
+function onLoad() {
+	for (var i = 0; i < 12; i++) {
+		console.log(i, seq);
+		const newSeq = [];
+		for (var j = 0; j < seq.length - 1; j++) {
+			newSeq.push(seq[j]);
+			newSeq.push(seq[j] + seq[j + 1]);
+		}
+		newSeq.push(seq[j]);
+		seq = newSeq;
+	}
+}
